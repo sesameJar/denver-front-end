f83896190c7832d53d36d375d4c4a872ecddb97c
Ben Weinberg <weinbergben33@gmail.com>
2021-02-09 12:53:06 -0500

    added video player component

---
+++ components/VideoPlayer.vue
@@ -0,0 +1,20 @@
+<template>
+  <video src="" />
+</template>
+
+<script>
+export default {
+  name: 'VideoPlayer',
+  props: {
+    videoData: {
+      type: String,
+      default: null
+    }
+  }
+
+}
+</script>
+
+<style>
+
+</style>
