08cbcce751d8f27a3af784d4944531d274e60c91
Ben Weinberg <weinbergben33@gmail.com>
2021-02-08 22:11:07 -0500

    added img to join

--- pages/JoinChallenge/index.vue
+++ pages/JoinChallenge/index.vue
@@ -1,10 +1,8 @@
 <template>
-  <div>
-    <div class="JoinChallenge-upload-video">
-      <div v-if="!videoUploaded" class="JoinChallenge-video-upload-container" style="margin-bottom:25px">
-        <div style="justify-content:center">
-          <p>test</p>
-        </div>
+  <div class="ChallengeCreation-upload-video">
+    <div class="ChallengeCreation-video-upload-container" style="margin-bottom:25px">
+      <div style="justify-content:center">
+        <img class="ChallengeCreation-dotted-camera-circle" src="https://cdn.iconscout.com/icon/free/png-256/video-842-475041.png" height="50px" width="50px">
       </div>
     </div>
   </div>
