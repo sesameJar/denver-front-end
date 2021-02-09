cc8d6f455a61ef212104d49b38065c15fb89c1b3
Lijia Hou <lijia.hou@mail.utoronto.ca>
2021-02-09 18:38:53 -0500

    added search bar in navbar and added styling

--- components/layout/Navbar.vue
+++ components/layout/Navbar.vue
@@ -1,5 +1,15 @@
 <template>
   <v-toolbar>
+    <v-toolbar
+      dense
+      floating
+    >
+      <v-text-field
+        hide-details
+        prepend-icon="mdi-magnify"
+        single-line
+      />
+    </v-toolbar>
     <div class="Navbar__container">
       <n-link
         class="Navbar__link"
@@ -26,11 +36,12 @@
 </template>
 <style scoped>
 .Navbar__container {
-  margin-left: 65%;
+  margin-left: 50%;
   display: flex;
   flex-direction: row;
   flex-grow: 5;
 }
+
 .Navbar__link {
   color: black;
   text-decoration: none;
