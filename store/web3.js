4b73c02827c4a0560b7836eedd037afa54bedf94
ehsueh <emma.yc.hsueh@gmail.com>
2021-02-10 17:58:25 -0500

    add startChallenge in web3 (wip - spinning!)

---
+++ store/web3.js
@@ -0,0 +1,102 @@
+import { ethers } from 'ethers'
+import starRelayJson from '@/truffle/StarRelay'
+import { CONTRACT_STATUS } from '@/utils/constants'
+
+// Response and error should all be formatted as
+// { status , response }
+function handleResponse (response) {
+  console.log(response)
+  return {
+    status: CONTRACT_STATUS.SUCCESS,
+    response
+  }
+}
+
+function handleError (error) {
+  console.log(error)
+  if (error.message.includes('insufficient funds')) {
+    return {
+      status: CONTRACT_STATUS.INSUFFICIENT_FUNDS,
+      response: error.message
+    }
+  }
+  if (error.message.includes('denied transaction signature')) {
+    return {
+      status: CONTRACT_STATUS.USER_REJECTED,
+      response: error.message
+    }
+  }
+  return {
+    status: CONTRACT_STATUS.UNCLASSIFIED_FAILURE,
+    response: error.message
+  }
+}
+
+function getContractAddress(truffleConf, chainId) {
+  if (!truffleConf || !chainId) { return '' }
+  const { networks } = truffleConf
+  if (networks[chainId.toString()]) {
+    const address = networks[chainId.toString()].address
+    return address || ''
+  }
+  return ''
+}
+
+export const state = () => ({
+  contract: null
+})
+
+export const getters = {
+  starRelayAddress: state =>
+    getContractAddress(starRelayJson, state.chainId),
+  starRelayContract: null
+}
+
+export const mutations = {
+  setProviderAndSigner (state, { provider, signer }) {
+    state.provider = provider
+    state.signer = signer
+  },
+  setContract (state, contract) {
+    state.starRelayContract = contract
+  }
+}
+
+export const actions = {
+  // Boostrap
+  async bootstrap (vuexStore, web3) {
+    const { commit } = vuexStore
+    try {
+      const provider = new ethers.providers.Web3Provider(web3.currentProvider)
+      const signer = provider.getSigner()
+      commit('setProviderAndSigner', { provider, signer })
+      const chain = await provider.getNetwork()
+      const starRelayAddress = getContractAddress(
+        starRelayJson,
+        chain.chainId
+      )
+      const starRelay = new ethers.Contract(
+        starRelayAddress,
+        starRelayJson.abi,
+        signer
+      )
+      commit('setContract', starRelay)
+    } catch (e) {
+      console.log(e)
+    }
+  },
+  // Start a challenge
+  startChallenge (vuexStore, params) {
+    const { state } = vuexStore
+    const { beneficiary, invitedAddresses, endTimestamp, minEntryFee, ipfsHash } = params
+    return state.starRelayContract
+      .startChallenge(
+        beneficiary,
+        invitedAddresses,
+        endTimestamp,
+        minEntryFee,
+        ipfsHash)
+      .then(res => handleResponse(res))
+      .catch(error => handleError(error))
+  }
+}
