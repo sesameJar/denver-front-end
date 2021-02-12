d9d8a519d84919108cd1c8fe5bde832995704a08
ehsueh <emma.yc.hsueh@gmail.com>
2021-02-12 02:42:47 -0500

    make challenge cards as v-cards

--- components/ChallengeCard.vue
+++ components/ChallengeCard.vue
@@ -1,21 +1,31 @@
 <template>
-  <div v-if="challenge && challenge.videos && challenge.videos.length">
-    <nuxt-link :to="`/challenge/${challenge.id}`">
-      <VideoPlayer
-        :video-data="challenge.videos[0].id"
-      />
-      <h3> {{ challenge.title }} </h3>
-      <div
-        v-if="challenge.endTimestamp > 0"
-      >
-        <p class="ChallengeCard__time">
-          {{
-            new Date(challenge.endTimestamp * 1000) | moment("from")
-          }}
-        </p>
-      </div>
-    </nuxt-link>
-  </div>
+  <v-card v-if="challenge && challenge.videos && challenge.videos.length" max-width="480" elevation="2">
+    <n-link :to="`/challenge/${challenge.id}`">
+      <v-card-text>
+        <VideoPlayer
+          :video-data="challenge.videos[0].id"
+        />
+      </v-card-text>
+      <v-card-title>
+        <h3> {{ challenge.title }} </h3>
+      </v-card-title>
+      <v-card-subtitle>
+        <div
+          v-if="challenge.endTimestamp > 0"
+        >
+          <p class="ChallengeCard__time">
+            {{
+              new Date(challenge.endTimestamp * 1000) | moment("from")
+            }}
+          </p>
+        </div>
+      </v-card-subtitle>
+      <!-- TODO can add this in when user have usernames -->
+      <!-- <v-card-actions>
+        <Account :account="challenge.creator" />
+      </v-card-actions> -->
+    </n-link>
+  </v-card>
 </template>
 <script>
 import VideoPlayer from '@/components/VideoPlayer'
